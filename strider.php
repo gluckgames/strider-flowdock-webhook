78864ba5dd9ef553c171ccb478c5f044a90e09b4
Marek Ventur <marek@debian>
2013-09-18 13:51:14 +0000

    Initial commit

---
+++ strider.php
@@ -0,0 +1,55 @@
+<?php
+	/*
+		Add a webhook for each project like this:
+		Hook Title: Flowdock
+		TargetURL for HTTP Post: http://wherever.this.file.is.hosted.com/strider.php?repo=repoName
+		Shared secret for HMAC-SHA1 signatire: <doesn't matter>
+	*/
+
+	/*
+	Define channels like this in config.php
+	$flowdockChannels = array(
+	    // this has to match the repo name you define in the webhook:
+		'repoName' => 
+			array(
+				'flowdockUrl' => 'https://api.flowdock.com/v1/messages/chat/<yourFlowdockToken>',
+				'striderUrl' => 'http://strider.yourcompany.com/yourProject/yourRepo/'
+			)
+	);
+	*/
+	include 'config.php';
+
+	$payload = file_get_contents('php://input');
+	$value = json_decode($payload);
+
+	$testExitcode = $value->test_results->test_exitcode;
+	$repoUrl = $value->test_results->repo_url;
+	$commit = $value->test_results->github_commit_id;
+
+	function sendToFlowdock($text, $repo) {
+		global $flowdockChannels;
+		$striderUrl = $flowdockChannels[$repo]['striderUrl'];
+		$flowdockUrl = $flowdockChannels[$repo]['flowdockUrl'];
+
+		$payload = array(
+			"content" => $text . ' ' . $striderUrl,
+			"external_user_name" => "Strider",
+			"tags" => array("#strider")
+		);
+
+		$json = json_encode($payload);
+
+        $curl = curl_init(); 
+        curl_setopt($curl, CURLOPT_URL, $flowdockUrl); 
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('content-type:application/json')); 
+        curl_setopt($curl, CURLOPT_POST, true); 
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $json); 
+        curl_exec($curl); 
+        curl_close($curl); 
+	}	
+
+	if ($testExitcode == 0) {
+		sendToFlowdock("Build ✓", $_GET['repo']);
+	} else {
+		sendToFlowdock("Build ☢", $_GET['repo']);
+	}
